7269441bfd0d400da192e257335a4d54db8cfbbf
Newcoder <8m8opqt7@students.codeinstitute.net>
2025-02-27 12:03:23 +0100

    add auth and customer

---
+++ server/customer/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Customer
+from django.contrib.auth import authenticate
+
+class CustomerRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Customer
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        return Customer.objects.create_user(**validated_data)
+    
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        
+        attrs['user'] = user
+        return attrs
