b27b9dea5f5e1a620b2b03eead9df77a3b824840
Newcoder <8m8opqt7@students.codeinstitute.net>
2025-02-27 17:09:59 +0100

    deployment

--- server/server/settings.py
+++ server/server/settings.py
@@ -32,10 +32,7 @@ DEBUG = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000", 
-    "http://localhost:5173", 
-    "http://127.0.0.1:3000", 
-    "http://127.0.0.1:5173", 
+    "*"
 ]
 
 ALLOWED_HOSTS=['*']
