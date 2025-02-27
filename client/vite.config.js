b33e97d1a917b3846150ae6be20146b87d46f408
Newcoder <8m8opqt7@students.codeinstitute.net>
2025-02-27 18:33:46 +0100

    deployment issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { build, defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'build'
+  }
 });
