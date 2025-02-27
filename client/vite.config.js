62bb6cab755b8d7a3e2a0c2033614b7ce1b07c30
Newcoder <8m8opqt7@students.codeinstitute.net>
2025-02-27 18:36:05 +0100

    prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { build, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ["https://booksandbeyond-production.up.railway.app"]
   },
   build: {
     outDir: 'build'
